da33393b0799cbbfa5c23dca8d417aa71663dd9c
RudyMontenegro <rudymontenegro4@gmail.com>
2023-03-17 16:19:55 -0400

    Creado swith map auth para enviar otken y a la ves resivir profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 import { Product } from './models/product.model';
 
+import { AuthService } from '../app/services/auth.service';
+import { UsersService } from '../app/services/users.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,7 +13,16 @@ import { Product } from './models/product.model';
 export class AppComponent {
   imgParent = '';
   showImg = true;
+  token = '';
+  myInputMessage:string ="I am the parent comppnent"
 
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+    
+  ){
+
+  }
 
   onLoaded(img: string) {
     console.log('log padre', img);
@@ -19,4 +31,16 @@ export class AppComponent {
   toggleImg() {
     this.showImg = !this.showImg;
   }
+
+  createUser(){
+    this.usersService.create({ // crear la manera de conectar con la interfaz
+       name: 'Rudy',
+       email: 'rolidoss7@gmail.com',
+       password: '123123', 
+    })
+    .subscribe(rta => {
+      console.log(rta)
+    })
+  }
+  
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
-import { StoreService } from '../../services/store.service'
+import { StoreService } from '../../services/store.service';
+
+import { UsersService } from '../../services/users.service'
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -8,22 +13,44 @@ import { StoreService } from '../../services/store.service'
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
+  
+  token = ''
+  profile: User | null = null;
   activeMenu = false;
   counter = 0;
 
   constructor(
-    private storeService: StoreService
+    private storeService: StoreService,
+    private authService: AuthService,
   ) { }
 
   ngOnInit(): void {
     this.storeService.myCart$.subscribe(products => {
       this.counter = products.length;
+     
     });
   }
 
   toggleMenu() {
     this.activeMenu = !this.activeMenu;
   }
-
+  login(){
+    this.authService.loginAndGet('sebas@mail.com', '1212')
+  
+   .subscribe(rta => {
+     //console.log(rta.access_token);// crear modelo auth, poner en el service, tiparlo
+    //this.profile = rta;
+    
+    this.profile = rta;
+     this.token = '---'
+   })
+   this.getProfile();
+  }
+  getProfile(){
+    this.authService.getProfile(this.token).
+    subscribe(rta => {
+      this.profile = rta;
+      console.log(rta)
+    })
+  }
 }
